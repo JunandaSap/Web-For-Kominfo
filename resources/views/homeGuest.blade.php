06b660a47c5e6b6447bf6ba1969fb291c701a514
Junanda <saputrajunanda620@gmail.com>
2024-07-03 14:50:41 +0700

    Landing Page

--- resources/views/homeGuest.blade.php
+++ resources/views/homeGuest.blade.php
@@ -1,43 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>Washwiz</title>
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <link rel="stylesheet" href="{{ asset('css/homeGuest.css') }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/homeGuest.css') }}" rel="stylesheet">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">Kominfo Nganjuk</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Tentang</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Kontak</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/signup">Signup</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
 
-    <!-- Add your custom scripts here -->
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9gybBogGzR4V0QW1yfhXjr6ejb78hVJfSnV+hztK3f0nYDOMJU8" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-    <script>
-        document.getElementById('theme-toggle').addEventListener('click', function() {
-            document.body.classList.toggle('dark-theme');
-        });
-    </script>
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="#">BERANDA</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">PROFIL</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">GALERI</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">DOKUMEN</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+
+<div class="container">
+    <h1 class="section-title">BIDANG</h1>
+    <table class="section-table">
+        <tr class="section-content">
+            <td>
+                <h2>Bidang Layanan Pemerintah Berbasis Elektronik (e-Gov)</h2>
+                <p>Merupakan bidang yang menangani digitalisasi layanan...</p>
+                <ul>
+                    <li>Pembangunan dan pengembangan sistem informasi</li>
+                    <li>Penyediaan layanan berbasis digital</li>
+                    <li>Tata kelola dan evaluasi layanan elektronik</li>
+                </ul>
+            </td>
+            <td>
+                <img src="{{ asset('image/1719896242.png') }}" alt="e-Gov">
+            </td>
+        </tr>
+        <tr class="section-content">
+            <td>
+                <img src="{{ asset('image/1719896242.png') }}" alt="IT Infrastructure">
+            </td>
+            <td>
+                <h2>Bidang Keamanan dan Infrastruktur Teknologi Informasi</h2>
+                <p>Merupakan bidang yang menangani penyediaan dan pengelolaan...</p>
+                <ul>
+                    <li>Jaringan intra pemerintah</li>
+                    <li>CCTV</li>
+                    <li>Pusat Data</li>
+                    <li>Sistem Manajemen Keamanan Informasi</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+</div>
+<div>
+
+</div>
+<div class="footer">
+        <div class="social-icons">
+            <a href="https://www.google.com" target="_blank">Google</a>
+            <a href="https://www.microsoft.com" target="_blank">Microsoft</a>
+            <a href="https://aws.amazon.com" target="_blank">AWS</a>
+            <a href="https://www.ibm.com" target="_blank">IBM</a>
+            <a href="https://line.me" target="_blank">LINE</a>
+        </div>
+        <p>&copy; 2022-2023 | Dinas Komunikasi dan Informatika Kota Nganjuk</p>
+        <p>Alamat: Jl. Merdeka Nomor 21 Nganjuk, Nganjuk, East Java</p>
+        <p>Email: Kominfonganjuuk@gmail.com</p>
+        <p>Telepon: (031) 5312144 Psw. 384 / 241, (031) 99277339</p>
+    </div>
+
+<script src="{{ asset('js/jquery.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
 </html>
